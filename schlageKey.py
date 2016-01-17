fa0070e9472a933930158a7341469813051ca53c
colchuck <thefclef777>
2016-01-16 23:02:33 -0800

    Basic class structure

---
+++ schlageKey.py
@@ -0,0 +1,12 @@
+class schlageKey(object):
+    
+    def __init__(self, cutList = [0,0,0,0,0], numCuts=5):
+        self.cutList = cutList
+        self.numCuts = numCuts
+        
+    def getCuts(self):
+        return cutList
+    
+    def getNumCuts(self):
+        return numCuts
+        
