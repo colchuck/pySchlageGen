2e4a208874bd51176ad9a01159b8f3faf01f1355
colchuck <thefclef777>
2016-01-18 14:01:39 -0800

    added bitting calculator, added basic output

--- main.py
+++ main.py
@@ -9,5 +9,25 @@ mastKey = schlageKey.schlageKey(mastCuts,len(mastCuts))
 gen = schlageGen.schlageGen()
 gen.addMasterKey(mastKey)
 output = gen.genSystem(tenants)
+i=1
 for e in output:
-    print(e)
+    if i< 9:
+        o= "  " + str(i)
+    if i < 100 and i>9:
+        o= " " + str(i)
+    print(o + ":  ", end="")
+    for f in e:
+        print(str(f) + " ", end="")
+    print("\n--------------------")
+    bitting = gen.bittingCalc(e)
+    print("  M:  ",end="")
+    for f in bitting[0]:
+        if f==0:
+            f="x"
+        print(str(f) + " ",end="")
+    print()
+    print("  B:  ",end="")
+    for l in bitting[1]:
+        print(str(l) + " ",end="")
+    print("\n")
+    i=i+1
--- schlageGen.py
+++ schlageGen.py
@@ -11,7 +11,7 @@ class schlageGen(object):
         mastCuts = masterKey.getCuts()
         
     def genSystem(self,tenants):
-        mastCuts = self.masterKey.getCuts()
+        mastCuts = self.masterKey.getCuts()[:]
         tenantTemp = mastCuts[:]
         tenantSet = []
         j=0
@@ -26,9 +26,24 @@ class schlageGen(object):
                         return tenantSet
                 j=j+1
             j=f +1    
-            if not (mastCuts[f] + 2 >10 ): 
-                mastCuts[f] = mastCuts[f] + 2
+            if not (mastCuts[f] + 3 >=10 ): 
+                mastCuts[f] = mastCuts[f] + 3
             else:
                 f=f+1
+    
+    def bittingCalc(self,tenantKey):
+        tenCuts = tenantKey[:]
+        mastCuts = self.masterKey.getCuts()[:]
+        i=0
+        master=[]
+        bottom=[]
+        while i<len(mastCuts):
+            chamber = [mastCuts[i],tenCuts[i]]
+            bottom.append(min(chamber))
+            master.append(max(chamber) - min(chamber))
+            i=i+1
+        return [master,bottom]
+            
+        
         
         
